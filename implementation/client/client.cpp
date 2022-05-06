26c4c53a8663af7329a63a40c06ec2d2f37669c6
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-05-06 19:42:07 +0200

    Added basic socket and server/client interactions

--- implementation/client/client.cpp
+++ implementation/client/client.cpp
@@ -1,37 +1,56 @@
 #include <arpa/inet.h>
+#include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 8080
+
+#define PORT 8081
+#define ADDRESS "127.0.0.1"
+
+using namespace std;
+
+void interact(int server_fd) {
+    string input;
+    char buffer[1024] = {0};
+    cout << "What do you want to send to the server? ";
+    cin >> input;
+    write(server_fd, input.c_str(), input.length());
+    read(server_fd, buffer, sizeof(buffer));
+    cout << "From server: " << buffer << endl;
+}
 
 int main(int argc, char const *argv[]) {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char hello[] = "Hello from client";
-    char buffer[1024] = {0};
+
+    // Create the socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
+    // Set socket address and port
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary
     // form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+        perror("Cannot convert address");
+        exit(EXIT_FAILURE);
     }
 
+    // Connect to the server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
+        perror("Cannot connect to server");
+        exit(EXIT_FAILURE);
     }
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(sock, buffer, 1024);
-    printf("%s\n", buffer);
-    return 0;
+
+    // Start interacting with the server
+    interact(sock);
+
+    // Close socket when we are done
+    close(sock);
 }
--- implementation/server/server.cpp
+++ implementation/server/server.cpp
@@ -1,52 +1,99 @@
+#include <csignal>
+#include <iostream>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 8080
+#include <wait.h>
+#define PORT 8081
+
+using namespace std;
+
+pid_t server = -1;
+
+/* Handler for SIGINT. Gracefully shuts down the server by:
+ *     - waiting for every child to terminate (we assume that child processes
+ *       will eventually terminate)
+ */
+void sigint_handler(int signum) {
+    if (server == getpid()) {
+        cout << "Gracefully shutting down server..." << endl;
+
+        while (wait(NULL) > 0)
+            ;
+        cout << " Bye!" << endl;
+
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void serve_client(int client_fd) {
+    char test[] = "Hello from server!";
+    char buffer[1024] = {0};
+    write(client_fd, test, sizeof(test));
+    read(client_fd, buffer, sizeof(buffer));
+    cout << "From client: " << buffer << endl;
+}
 
 int main() {
-    int server_fd, new_socket, valread;
+    int sock, new_client, enable_sockopt = 1;
     struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char hello[] = "Hello from server";
+    socklen_t addr_len = sizeof(address);
+    pid_t res;
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    server = getpid();
+
+    // Register signal handler to gracefully close on SIGINT
+    signal(SIGINT, sigint_handler);
+
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable_sockopt,
+                   sizeof(int)) < 0) {
+        perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
+
+    // Set socket address and port
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    // Attach socket to specified port
+    if (bind(sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) {
+
+    // Start listening on it
+    if (listen(sock, 0) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
+
+    // Accept loop: each time a new client connects start a new process for that
+    // client. The child process will handle all interactions with the client.
+    while ((new_client =
+                accept(sock, (struct sockaddr *)&address, &addr_len)) >= 0) {
+        if ((res = fork()) == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (res == 0) {
+            serve_client(new_client);
+            // Close the file descriptor after we are done with it
+            close(new_client);
+            exit(EXIT_SUCCESS);
+        };
+
+        // The parent closes the fd immediately
+        close(new_client);
     }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    return 0;
+    perror("accept");
+    exit(EXIT_FAILURE);
 }
