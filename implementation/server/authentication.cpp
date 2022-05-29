ce9f02bb1e6bb5e491cd067e71526984f05af4a1
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-05-29 10:26:49 +0200

    Implemented something more out of boredom caused by a certain exam

--- implementation/server/authentication.cpp
+++ implementation/server/authentication.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 // Possible users of the server
 string users[2] = {"alice", "bob"};
+char server_name[] = "server";
 
 /* Reads all the public keys of the registered users */
 static map<string, EVP_PKEY *> setup_keys() {
@@ -31,27 +32,28 @@ static map<string, EVP_PKEY *> setup_keys() {
             (std::filesystem::canonical(".") / "certificates" / (user + ".pub"))
                 .string();
 
-#ifdef DEBUG
-        cout << "Loading public key for user: " << user << endl
-             << "Path: " << user_key_path << endl;
-#endif
-
-        // Open the public key
+        // Open the public key file
         if ((public_key_fp = fopen(user_key_path.c_str(), "r")) == nullptr) {
+            user_map.clear();
             perror("Cannot read user key");
             abort();
         }
 
-        // ... and save it into the list of users
+        // ... and read it as a public key
         if ((pubkey = PEM_read_PUBKEY(public_key_fp, nullptr, nullptr,
-                                      nullptr)) == nullptr)
+                                      nullptr)) == nullptr) {
+            user_map.clear();
             handle_errors();
+        }
 
         // ... and save it into the list of users
         user_map.insert({user, pubkey});
 
 #ifdef DEBUG
-        cout << "Loaded public key for " << user << endl;
+        cout << "Loaded public key for " << user << endl
+             << "Path: " << user_key_path << endl
+             << endl;
+        ;
 #endif
     }
 
@@ -67,7 +69,9 @@ unsigned char *authenticate(BIO *socket, int key_len) {
 
     // Check the correctness of the message type
     if (type != AuthStart) {
-        cerr << "Incorrect message type";
+        cerr << "Incorrect message type" << endl;
+        user_keys.clear();
+        BIO_free(socket);
         abort();
     }
 
@@ -85,13 +89,44 @@ unsigned char *authenticate(BIO *socket, int key_len) {
         auto client_pubkey = finder->second;
     } else {
         cerr << "User not registered!" << endl;
+        user_keys.clear();
+        BIO_free(socket);
         abort();
     }
 
-    // Load the client half key (TODO)
-    auto client_half_key = PEM_read_bio_PUBKEY(socket, NULL, NULL, NULL);
-    if (client_half_key == NULL)
+    // Load the client half key
+    BIO *tmp_bio;
+    if ((tmp_bio = BIO_new(BIO_s_mem())) == NULL) {
+        user_keys.clear();
+        BIO_free(socket);
         handle_errors();
+    }
+
+    // Read client half key in PEM format
+    // TODO: this can fail too possibly, therefore we need to check for errors
+    // and free memory correctly!
+    // See
+    // https://stackoverflow.com/questions/3157098/whats-the-right-approach-to-return-error-codes-in-c
+    // for an elegant solution to the problem!
+    auto [client_half_key_len, client_half_key_bin] = read_field<uchar>(socket);
+
+    // Write it to memory bio
+    if (BIO_write(tmp_bio, client_half_key_bin, client_half_key_len) !=
+        client_half_key_len) {
+        user_keys.clear();
+        BIO_free(socket);
+        BIO_free(tmp_bio);
+        handle_errors();
+    }
+
+    // ... and extract it as the client half key
+    auto client_half_key = PEM_read_bio_PUBKEY(tmp_bio, NULL, NULL, NULL);
+    if (client_half_key == NULL) {
+        user_keys.clear();
+        BIO_free(socket);
+        BIO_free(tmp_bio);
+        handle_errors();
+    }
 
 #ifdef DEBUG
     cout << "Received PEM pubkey: " << endl;
@@ -99,6 +134,7 @@ unsigned char *authenticate(BIO *socket, int key_len) {
 #endif
 
     // Send server name ("server")
+    send_field(socket, sizeof(server_name), server_name);
 
     // Send server half key
 
